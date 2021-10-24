5b5f58ca350f6b7a5292c205879bbfaa1b5f5b7b
Jung Hyun Park <rootjh937@gmail.com>
2021-10-24 20:52:56 +0900

    코드 수정 ( 한국 주식 )

--- cloud_function/koreaStock.py
+++ cloud_function/koreaStock.py
@@ -2,15 +2,32 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import datetime
-import time
+
+# GLOBAL VARIABLE
+SAVE_API_URL = 'http://localhost:9090/api/korea_stock'
+SAVE_API_HEADERS = {
+    'Content-Type': 'application/json',
+    'api_token': 'Pvk4coiwHqt3ZqQ8SfJD!'
+}
+CRAWLING_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
+}
+TARGET_CRAWLING_SETTING = {
+    'kospi': {
+        'name': 'KOSPI',
+        'url': 'https://kr.investing.com/indices/kospi-chart',
+        'from': 'INVESTING'
+    },
+    'samsung': {
+        'name': 'SAMSUNG',
+        'url': 'https://www.google.com/finance/quote/005930:KRX',
+        'from': 'GOOGLE'
+    }
+}
+
 
 def save_data(request_body):
-    url = "http://localhost:9090/api/korea_stock"
-    headers = {
-        'Content-Type': 'application/json',
-        'api_token': 'Pvk4coiwHqt3ZqQ8SfJD!'
-    }
-    save_response = requests.post(url=url, headers=headers, data=json.dumps(request_body))
+    save_response = requests.post(url=SAVE_API_URL, headers=SAVE_API_HEADERS, data=json.dumps(request_body))
     if save_response.status_code == 200:
         return True
     else:
@@ -25,12 +42,9 @@ def make_request_body(stock_code, stock_price, from_data, timestamp):
         "timestamp": timestamp
     }
 
+
 def get_kospi_investing_crawling():
-    url = "https://kr.investing.com/indices/kospi-chart"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
-    }
-    raw_crawling = requests.get(url=url, headers=headers)
+    raw_crawling = requests.get(url=TARGET_CRAWLING_SETTING['kospi']['url'], headers=CRAWLING_HEADERS)
     if raw_crawling.status_code == 200:
         html = raw_crawling.text
         soup = BeautifulSoup(html, 'html.parser')
@@ -38,52 +52,63 @@ def get_kospi_investing_crawling():
             '#last_last'
         ).text
         kospi_number = float(kospi_str.replace(",", ''))
-        return True, 'KOSPI', kospi_number, int(datetime.datetime.now().timestamp())
+        return [kospi_number, int(datetime.datetime.now().timestamp())]
     else:
-        return False, 'KOSPI', 0, 0
+        return []
 
 
 def get_samsung_google_crawling():
-    url = "https://www.google.com/finance/quote/005930:KRX"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
-    }
-    raw_crawling = requests.get(url=url, headers=headers)
+    raw_crawling = requests.get(url=TARGET_CRAWLING_SETTING['samsung']['url'], headers=CRAWLING_HEADERS)
     if raw_crawling.status_code == 200:
         html = raw_crawling.text
         soup = BeautifulSoup(html, 'html.parser')
         samsung_str = soup.find_all('div', 'YMlKec fxKbKc')[0].text
         samsung_number = float(samsung_str.replace(",", '').replace("₩", ''))
-        return True, 'SAMSUNG', samsung_number, int(datetime.datetime.now().timestamp())
+        return [samsung_number, int(datetime.datetime.now().timestamp())]
         return
     else:
-        return False, 'SAMSUNG', 0, 0
+        return []
 
-def main():
+def main(args):
     final_return_value = {
         'payload': 'get_korea_stock'
     }
-    kospi_check_status, kospi_code, kospi_price, kospi_timestamp = get_kospi_investing_crawling()
-    if kospi_check_status:
-        kospi_request_body = make_request_body(kospi_code, kospi_price, 'INVESTING', kospi_timestamp)
-        if save_data(kospi_request_body):
-            final_return_value['KOSPI'] = 'success'
-        else:
-            final_return_value['KOSPI'] = 'save error'
-    else:
-        final_return_value['KOSPI'] = 'call error'
 
-    samsung_check_status, samsung_code, samsung_price, samsung_timestamp = get_samsung_google_crawling()
-    if samsung_check_status:
-        samsung_request_body = make_request_body(samsung_code, samsung_price, 'GOOGLE', samsung_timestamp)
-        if save_data(samsung_request_body):
-            final_return_value['SAMSUNG'] = 'success'
+    # KOSPI
+    kospi_name = TARGET_CRAWLING_SETTING['kospi']['name']
+    kospi_data = get_kospi_investing_crawling()
+    if len(kospi_data) > 0:
+        kospi_request_body = make_request_body(
+            kospi_name,
+            kospi_data[0],
+            TARGET_CRAWLING_SETTING['kospi']['from'],
+            kospi_data[1]
+        )
+        if save_data(kospi_request_body):
+            final_return_value[kospi_name] = 'success'
         else:
-            final_return_value['SAMSUNG'] = 'save error'
+            final_return_value[kospi_name] = 'save error'
     else:
-        final_return_value['SAMSUNG'] = 'call error'
+        final_return_value[kospi_name] = 'call error'
+
+    # SAMSUNG
+    samsung_name = TARGET_CRAWLING_SETTING['samsung']['name']
+    samsung_data = get_samsung_google_crawling()
+    if len(samsung_data) > 0:
+        samsung_request_body = make_request_body(
+            samsung_name,
+            kospi_data[0],
+            TARGET_CRAWLING_SETTING['samsung']['from'],
+            kospi_data[1]
+        )
+        if save_data(samsung_request_body):
+            final_return_value[samsung_name] = 'success'
+        else:
+            final_return_value[samsung_name] = 'save error'
+    else:
+        final_return_value[samsung_name] = 'call error'
 
     return final_return_value
 
 
-print(main())
+print(main(0))
